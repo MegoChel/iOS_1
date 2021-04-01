692091c17806510f4cddffc54818badbb82676ac
Ivan <meg4el.ivan@gmail.com>
2021-04-02 03:07:56 +0700

    init2

---
+++ 111/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  111
+//
+//  Created by Admin on 18.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //self.navigationController?.isNavigationBarHidden = true
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
